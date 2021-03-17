dc602c69a82701df684f8f0fe7f2cf12bfa0d978
spiguer <nunoecb@gmail.com>
2021-03-17 02:45:57 +0000

    adicionar humidade

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback ) =>{
         }else if(body.error){
             callback('Unable to find location', undefined)
         }else{
-            callback(undefined, 'Tempuratura: '+body.current.temperature + ' Descrição: '+body.current.weather_descriptions + ' Sensação térmica: '+body.current.feelslike)
+            callback(undefined, 'Tempuratura: '+body.current.temperature + ' Descrição: '+body.current.weather_descriptions + '- Sensação térmica: '+body.current.feelslike + '- Humidade: '+body.current.humidity + '- Velocidade vento: '+body.current.wind_speed)
         }
     })
 }
